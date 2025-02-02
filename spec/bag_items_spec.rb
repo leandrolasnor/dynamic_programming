97fe71f133043f67aada4783e3a024ab6a00ebb0
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2025-02-02 16:45:55 -0300

    test: improve test case

--- spec/bag_items_spec.rb
+++ spec/bag_items_spec.rb
@@ -1,15 +1,6 @@
 require './bag_items.rb'
 
 RSpec.describe BagItems do
-  let(:items) do
-    [
-      Item.new(weight: 1, profit: 1),
-      Item.new(weight: 3, profit: 5),
-      Item.new(weight: 5, profit: 8),
-      Item.new(weight: 8, profit: 10)
-    ]
-  end
-
   context 'on initializer' do
     context 'when items param is a empty array' do
       let(:bag_items) { described_class.new(backpack: Backpack.new(size: 11)) }
@@ -22,9 +13,18 @@ RSpec.describe BagItems do
   end
 
   context 'on Success' do
+    let(:full_backpack) { described_class.call(backpack: Backpack.new(size: size), items: items) }
+    let(:items) do
+      [
+        Item.new(weight: 1, profit: 1),
+        Item.new(weight: 3, profit: 5),
+        Item.new(weight: 5, profit: 8),
+        Item.new(weight: 8, profit: 10)
+      ]
+    end
+
     context 'when backpack size is 11' do
       let(:size) { 11 }
-      let(:full_backpack) { described_class.call(backpack: Backpack.new(size: size), items: items) }
 
       it 'must to return a full backpack' do
         expect(full_backpack.items.size).to eq(2)
@@ -37,7 +37,6 @@ RSpec.describe BagItems do
 
     context 'when backpack size is 10' do
       let(:size) { 10 }
-      let(:full_backpack) { described_class.call(backpack: Backpack.new(size: size), items: items) }
 
       it 'must to return a full backpack' do
         expect(full_backpack.items.size).to eq(3)
